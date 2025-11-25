bdb84b99c569e02f664c311ef459d2d93594a9fc
sharp-lilac <jessicacboles@gmail.com>
2025-11-25 14:21:32 -0600

    Add protected areas shapefiles

--- helper_scripts/shapefiles.r
+++ helper_scripts/shapefiles.r
@@ -3,3 +3,5 @@
 # Load Shapefiles ---------------------------
 belize_map <- st_read("shapefiles/Belize_Basemap.shp") %>%
     st_transform(4326)
+protected_map <- st_read("shapefiles/ProtectedAreas_2024.shp") %>%
+    st_transform(4326)
